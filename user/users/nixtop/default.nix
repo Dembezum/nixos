081841b78d9ea7d77f121e22b11fd6d101d0845f
dembezum <dembezuuma@gmail.com>
2024-02-27 21:24:26 +0100

    update flake and user

--- user/users/nixtop/default.nix
+++ user/users/nixtop/default.nix
@@ -1,17 +1,32 @@
-{ config, pkgs, libs, ... }:
+#{
+#  imports = [
+##     ./user/modules/gnome
+##     ./user/modules/kitty
+#      ./user/modules/neovim
+#      ./user/modules/normie
+#      ./user/modules/shells
+#      ./home.nix
+##      ./user/modules/tmux
+## Add any other user module or configuration
+#  ];
+#
+#}
+#
+#
+
+{ config, pkgs, ... }:
 
 {
   imports = [
-#     ./user/modules/gnome
-#     ./user/modules/kitty
-      ./user/modules/neovim
-      ./user/modules/normie
-      ./user/modules/shells
-      ./home.nix
-#      ./user/modules/tmux
-# Add any other user module or configuration
+    ../../../user/modules/neovim/default.nix
+    ../../../user/modules/normie/default.nix
+    ../../../user/modules/shells/bash.nix
+    ./home.nix
+    # Add any other user module or configuration as needed
+    # Uncomment other imports as necessary
+    # ../../../user/modules/gnome/default.nix
+    # ../../../user/modules/kitty/default.nix
+    # ../../../user/modules/tmux/default.nix
   ];
-
 }
-
 
