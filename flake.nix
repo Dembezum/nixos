30ed36f8a14b3a83d39d0ffd7ba3e03e99992bfd
dembezum <dembezuuma@gmail.com>
2024-02-27 20:36:00 +0100

    .

--- flake.nix
+++ flake.nix
@@ -16,16 +16,25 @@
       nixosConfigurations = {
         nixos = lib.nixosSystem {
           inherit system;
-          modules = [ ./configuration.nix ];
+          modules = [ ./system/modules ];
         };
       };
       homeConfigurations = {
         nixtop = home-manager.lib.homeManagerConfiguration { 
           inherit pkgs;
           modules = [
-            ./nixtop
+            ./user/modules/gnome
+            ./user/modules/neovim
+            ./user/modules/normie
+            ./user/modules/shells
+            # Add any other user modules or configurations here
           ];
+          users = {
+            nixtop = ./user/users/nixtop;
+            # Add any other user configurations here
+          };
         };
       };
     };
 }
+
