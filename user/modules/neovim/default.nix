9a4bcef45cfbaeb45b6901b212675468802f439a
dembezum <dembezuuma@gmail.com>
2024-02-27 21:32:31 +0100

    update neovim

--- user/modules/neovim/default.nix
+++ user/modules/neovim/default.nix
@@ -1,32 +1,32 @@
 { config, lib, pkgs, ... }:
 
 let
-  # Define custom configurations or packages here, if necessary.
+# Define custom configurations or packages here, if necessary.
 in
 {
-  # Ensure Neovim is enabled and configured according to your preferences.
+# Ensure Neovim is enabled and configured according to your preferences.
   programs.neovim = {
     enable = true;
-    # Add more configuration options here as needed.
+# Add more configuration options here as needed.
   };
 
-  # Example: Adding custom plugins
-  # programs.neovim.plugins = with pkgs.vimPlugins; [
-  #   {
-  #     plugin = example-plugin;
-  #     config = ''
-  #       " Plugin-specific configurations here
-  #     '';
-  #   },
-  # ];
+# Example: Adding custom plugins
+# programs.neovim.plugins = with pkgs.vimPlugins; [
+#   {
+#     plugin = example-plugin;
+#     config = ''
+#       " Plugin-specific configurations here
+#     '';
+#   },
+# ];
 
-  # Ensure desired packages are available in the user environment.
+# Ensure desired packages are available in the user environment.
   home.packages = with pkgs; [
     lua-language-server
-    rust-analyzer
-    neovim
-    ripgrep
-    lua-language-server 
+      rust-analyzer
+      neovim
+      ripgrep
+      lua-language-server 
       rust-analyzer 
       neovim
       ripgrep
@@ -34,9 +34,5 @@ in
       gopls 
       clang-tools
       zls
-
-      nodePackages_latest.bash-language-server
-      nodePackages_latest.yaml-language-server
-    # Add other packages as necessary, ensuring there's a comma after each item except the last one.
   ];
 }
