46e3a69ad84ccb2522143704d012398ff04b27ef
dembezum <dembezuuma@gmail.com>
2024-02-28 15:25:29 +0100

    update kitty

--- user/modules/kitty/default.nix
+++ user/modules/kitty/default.nix
@@ -1,11 +1,21 @@
 { config, pkgs, ... }: {
-  # Ensure Program is installed by declaring it in the home.packages
+# Ensure Program is installed by declaring it in the home.packages
   home.packages = with pkgs; [
-    neovim
+    kitty
   ];
 
 # Kitty configuration
-programs.kitty.enable = true;
-programs.kitty.font.name = "JetBrainsMono Nerd Font";
-programs.kitty.theme = "Kurayami";
+  programs.kitty.enable = true;
+  programs.kitty.font.name = "JetBrainsMono Nerd Font Mono";
+  programs.kitty.theme = "Kurayami";
+
+# Adjusting keybindings to include font size changes
+  programs.kitty.keybindings = {
+    "ctrl+shift+enter" = "new_window";
+    "ctrl+shift+c" = "copy_to_clipboard";
+    "ctrl+shift+v" = "paste_from_clipboard";
+    "ctrl+shift+plus" = "increase_font_size";
+    "ctrl+shift+minus" = "decrease_font_size";
+  };
 }
+
