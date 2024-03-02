4ce983e0a18cf066d86626a16aa855b510d5ac0d
dembezum <dembezuuma@gmail.com>
2024-03-02 21:51:47 +0100

    update hyprland

--- user/modules/hyprland/default.nix
+++ user/modules/hyprland/default.nix
@@ -1,41 +1,36 @@
-{ config, pkgs, ... }:
+{ config, lib, inputs, pkgs, ... }:
+# modules/hyprland/default.nix
 
 {
+# Ensure desired packages are available in the user environment.
+  home.packages = with pkgs; [
+    gnome.gdm
+      rofi-wayland
+      libnotify
+      dunst
+      waybar
+      swww
+  ];
+
 # Xdg portals
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
   };
 
-# Neededc services 
+# Needed services 
   services = {
     xserver = {
       enable = true; 
       displayManager.gdm.enable = true;
     };
+
+# Gnome stuff
     gnome.gnome-keyring.enable = true;
 
-# gnome stuff
-    gnome.gnome-keyring.enable = true;
+# Opengl
+    hardware = {
+      Opengl
+        opengl.enable = true;
 
-    programs = {
-      dconf.enable = true;
-      direnv.enable = true;
-      hyprland = {
-        enable = true; 
-        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-      };
-      thunar = {
-        enable = true;
-        plugins = with pkgs.xfce; [ 
-          xfconf
-          thunar-volman 
-        ];
-      };
-    };
-
-# Ensure desired packages are available in the user environment.
-    home.packages = with pkgs; [
-      gnome.gdm
-    ];
-  }
+    }
