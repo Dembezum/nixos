5f66b38d39c2b40fbad7753493b98cd3cba63cdd
dembezum <dembezuuma@gmail.com>
2024-03-06 18:59:17 +0100

    add new profile

---
+++ profiles/virtual/configuration.nix
@@ -0,0 +1,56 @@
+{ pkgs, inputs, userSettings, ... }:
+
+{
+  imports = [
+# --- SYSTEM CONFIGURATION ---
+#       ./home.nix
+       ./hardware-configuration.nix
+      ../../universal.nix
+      ../../system/modules/networking
+      ../../system/modules/X11
+      ../../system/modules/bluetooth
+      ../../system/modules/pipewire
+#      ../../system/modules/virtualization
+# --- USER CONFIGURATION ---
+#      ../../user/modules/tmux
+#      ../../user/modules/kitty
+#      ../../user/modules/neovim
+#      ../../user/modules/shells
+#   ../../../user/modules/hyprland
+#    ../../../user/modules/normie
+#   ../../../user/modules/gnome
+#   ../../../user/modules/kde
+# ../../../user/modules/gtk
+  ];
+
+# Printer
+#  services.printing.enable = true;
+
+# Nix options.
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+# Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  users.users.${userSettings.username} = {
+    isNormalUser = true;
+    description = userSettings.name;
+    extraGroups = [ "video" "networkmanager" "wheel" ];
+    uid = 1000;
+  };
+  programs.hyprland.enable = true;
+hardware.opengl.enable = true;
+
+#  xdg.portal = {
+#    enable = true;
+#    extraPortals = [
+#      pkgs.xdg-desktop-portal
+#      pkgs.xdg-desktop-portal-gtk
+#    ];
+#  };
+
+  system.stateVersion = "23.11";
+}
+
---
+++ profiles/virtual/home.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, userSettings, ... }:
+
+{
+# -- IMPORTS --
+  imports = [
+    ../../user/modules/tmux
+      ../../user/modules/kitty
+      ../../user/modules/neovim
+      ../../user/modules/shells
+#   ../../../user/modules/hyprland
+#    ../../../user/modules/normie
+#   ../../../user/modules/gnome
+#   ../../../user/modules/kde
+# ../../../user/modules/gtk
+#      ./home.nix
+  ];
+
+# -- USER SETTINGS --
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/"+userSettings.username;
+  programs.home-manager.enable = true;
+
+# Package configuration
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  home.stateVersion = "23.11";
+
+
+# -- DEFAULT PACKAGES --
+  home.packages = with pkgs; [
+    firefox
+#      vesktop
+      gnome.gdm
+#      spotify
+#  rofi
+# Hyprland
+#      rofi-wayland
+#      libnotify
+#      dunst
+#      drm_info
+#      waybar
+#      swww
+#      wlroots
+#      wl-clipboard
+#      wdisplays
+#      wlr-randr
+#      xdg-desktop-portal-wlr
+#      grimblast
+  ];
+
+
+#  wayland.windowManager.hyprland = {
+#    enable = true;
+#    xwayland.enable = true;
+#    settings = {
+#      "$mod" = "SUPER";
+#      bind =
+#        [
+#        "$mod SHIFT, B, exec, firefox"
+#        "$mod SHIFT, Q, killactive"
+#        "$mod X, exec, kitty"
+#        ", Print, exec, grimblast copy area"
+#        ];
+#    };
+#    extraConfig = ''
+#      env = WLR_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0;
+#    '';
+#  };
+
+
+# -- VARIABLES --
+  home.sessionVariables = {
+    EDITOR = userSettings.editor;
+    TERM = userSettings.term;
+    BROWSER = userSettings.browser;
+  };
+
+# -- XDG CONFIGURATION --
+  xdg.enable = true;
+  xdg.userDirs = {
+    extraConfig = {
+      XDG_GAME_DIR = "${config.home.homeDirectory}/Games";
+    };
+  };
+
+}
