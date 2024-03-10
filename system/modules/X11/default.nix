36bf643cb983741759839afc1831463d497ff570
dembezum <dembezuuma@gmail.com>
2024-03-10 14:27:18 +0100

    update x11

--- system/modules/X11/default.nix
+++ system/modules/X11/default.nix
@@ -12,7 +12,7 @@
 
 #services.xserver.enable = true;
 #services.xserver.displayManager.sddm.enable = true;
-services.xserver.desktopManager.plasma6.enable = true;
+#services.xserver.desktopManager.plasma6.enable = true;
 
 #qt = {
 #  enable = true;
