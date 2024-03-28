33d53ca1954f76e84d7b6dc47f42db793c99768f
dembezum <dembezuuma@gmail.com>
2024-03-28 11:55:55 +0100

    add NixVpn

---
+++ profiles/vpn/configuration.nix
@@ -0,0 +1,80 @@
+{ systemSettings, userSettings, pkgs, ... }:
+
+# --- VPN CONFIGURATION ---
+{
+  imports = [
+      ../../universal.nix
+      ../../user/modules/tmux
+      ../../user/modules/shells
+      ./hardware-configuration.nix
+      ../../system/modules/networking
+  ];
+
+  environment.systemPackages = [ 
+    pkgs.tailscale 
+    pkgs.neovim
+  ];
+
+  programs.neovim = {
+    enable = true;
+    configure = {
+      customRC = ''
+        set cc=80
+        set number
+        set relativenumber
+        set hlsearch = false
+        set incsearch = true
+        set swapfile = false
+        set cursorline = true
+        set virtualedit = "block"
+        endif
+        '';
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [ ctrlp ];
+      };
+    };
+  };
+
+  services.tailscale.enable = true;
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = with pkgs; ''
+# wait for tailscaled to settle
+      sleep 2
+
+# check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+          fi
+
+# otherwise authenticate with tailscale
+          ${tailscale}/bin/tailscale up -authkey tskey-auth-kN5XLi5odW11CNTRL-akQPxKDChxfRxgsu53qsxfbPDDBYTWYFW
+          '';
+
+# Nix options.
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree = true;
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    users.users.${userSettings.username} = {
+      isNormalUser = true;
+      description = userSettings.name;
+      extraGroups = [ "plugdev" "networkmanager" "wheel" ];
+      uid = 1000;
+    };
+
+    system.stateVersion = systemSettings.systemstate;
+  };
+
+}
--- user/modules/tmux/default.nix
+++ user/modules/tmux/default.nix
@@ -1,13 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 
 
 {
-  home.packages = with pkgs; [
-# ...
-    tmux
-# ...
-  ];
-
   programs.tmux = {
     enable = true;
     prefix = "C-a";
