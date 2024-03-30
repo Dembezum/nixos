77fc2aee0bc793e9d60e3c9adcbcec70c2734093
dembezum <dembezuuma@gmail.com>
2024-03-30 10:08:39 +0100

    remove hypr from home-manager

--- user/modules/hyprland/default.nix
+++ user/modules/hyprland/default.nix
@@ -2,11 +2,7 @@
 # modules/hyprland/default.nix
 
 {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = false;
-  };
-  
+
   home.packages = with pkgs; [
     gnome.gdm
       rofi-wayland
