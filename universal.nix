3cbad8014ffbdc3672f23ecd90e615632ca81af1
dembezum <dembezuuma@gmail.com>
2024-04-01 20:46:00 +0200

    add swaylock

--- universal.nix
+++ universal.nix
@@ -81,7 +81,8 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 3d";
+
   };
 
   nixpkgs.config.allowUnfree = true;
--- user/modules/hyprland/default.nix
+++ user/modules/hyprland/default.nix
@@ -6,6 +6,7 @@
   imports = [
     inputs.nix-colors.homeManagerModules.default
      ./waybar.nix
+     ./swaylock
      ./swaync
      ./wlogout
      ../foot
