09d67cf8757b7a07482f38cb5109b6f79ab810a9
dembezum <dembezuuma@gmail.com>
2024-04-01 12:38:17 +0200

    update gtk

--- user/modules/gtk/default.nix
+++ user/modules/gtk/default.nix
@@ -1,37 +1,58 @@
-{ 
-  pkgs, 
-    custom ? {
-      font = "JetBrainsMono Nerd Font";
-      fontsize = "12";
-      cursor = "Numix-Cursor";
-    },
-    ... 
-}:
-{
-  gtk = {
-    enable = true;
-    font.name = "${custom.font} ${custom.fontsize}";
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.catppuccin-papirus-folders;
-    };
-    cursorTheme = {
-      name = "${custom.cursor}";
-      package = pkgs.numix-cursor-theme;
-    };
-    theme = {
-      name = "Catppuccin-Mocha-Standard-Blue-Dark";
-      package = pkgs.catppuccin-gtk.override { variant="mocha"; };
-    };
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-        '';
-    };
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-        '';
+{pkgs, ...}: {
+  home = {
+    packages = with pkgs; [
+      dconf
+        catppuccin-gtk
+        tree
+# gnome.dconf-editor
+    ];
+
+    pointerCursor = {
+      name = "Catppuccin-Mocha-Peach-Cursors";
+      package = pkgs.catppuccin-cursors.mochaPeach;
+      size = 24;
+      gtk.enable = true;
+      x11.enable = true;
     };
   };
-}
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["lavender"];
+        size = "standard";
+        tweaks = [];
+        variant = "mocha";
+      };
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+    gtk3 = {
+      extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+    };
+    gtk4 = {
+      extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+    };
+  };
+
+  home.sessionVariables = {
+    GTK_THEME = "Catppuccin-Mocha-Standard-Lavender-Dark";
+  };
+
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
+  };
+             }
