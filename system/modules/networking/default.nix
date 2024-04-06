b65c5e72426fa2e2d7422b8c0e816f895a0a8cfe
dembezum <dembezuuma@gmail.com>
2024-04-07 00:53:22 +0200

    add glances

--- system/modules/networking/default.nix
+++ system/modules/networking/default.nix
@@ -12,8 +12,8 @@
 # Firewall
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 22 443 8384 22000 ];
-    allowedUDPPorts = [ 22000 21027 ];
+    allowedTCPPorts = [ 22 443 8384 22000 61208 ];
+    allowedUDPPorts = [ 22000 21027 61208 ];
 
   };
 
