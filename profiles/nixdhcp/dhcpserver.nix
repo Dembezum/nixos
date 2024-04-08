c2bf15a71500e2e0a0d28be6a18a525c4da49227
dembezum <dembezuuma@gmail.com>
2024-04-08 12:17:16 +0200

    update dhcp server

--- profiles/nixdhcp/dhcpserver.nix
+++ profiles/nixdhcp/dhcpserver.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ ... }:
 
 {
 	services.kea = {
@@ -9,15 +9,23 @@
 						"ens18"
 					];
 				};
+
+				# Lease databse setup
 				lease-database = {
 					name = "/var/lib/kea/dhcp4.leases";
 					persist = true;
 					type = "memfile";
 				};
+
+				# Global values 
+				valid-lifetime = 4000;
 				rebind-timer = 2000;
 				renew-timer = 1000;
+
+				# Subnet configuration
 				subnet4 = [
 				{
+					subnet = "192.168.0.0/24";
 					pools = [
 					{
 						pool = "192.168.0.100 - 192.168.0.240";
@@ -25,10 +33,8 @@
 					];
 					 option-data = [
               { name = "routers"; data = "192.168.0.1"; }
-					 { name = "domain-name-servers"; data = "1.1.1.1, 1.0.0.1"; }
+    					{ name = "domain-name-servers"; data = "1.1.1.1, 1.0.0.1"; }
             ];
-					subnet = "192.168.0.0/24";
-					valid-lifetime = 4000;
 				}
 				];
 			};
