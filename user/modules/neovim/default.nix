b1bdf5807b19271ae67ed2100df7be4eeb9bfa33
dembezum <dembezuuma@gmail.com>
2024-04-11 10:26:54 +0200

    update neovim

--- user/modules/neovim/default.nix
+++ user/modules/neovim/default.nix
@@ -10,7 +10,14 @@
     vimdiffAlias = true;
   };
 
-    home.packages = with pkgs; [
-      lua-language-server
+  home.packages = with pkgs; [
+    lua-language-server
+      nixd
+      nodePackages_latest.vscode-langservers-extracted 
+      nodePackages_latest.vscode-langservers-extracted
+      nodePackages_latest.typescript-language-server 
+      nodePackages_latest.bash-language-server
+      nodePackages_latest.dockerfile-language-server-nodejs
+      nodePackages_latest.yaml-language-server
     ];
 }
