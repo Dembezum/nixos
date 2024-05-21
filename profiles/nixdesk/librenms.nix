dc77a820608a7f08b5f9b492022c10f8a349910c
dembezum <dembezuuma@gmail.com>
2024-05-21 18:23:57 +0200

    update librenms

--- profiles/nixdesk/librenms.nix
+++ profiles/nixdesk/librenms.nix
@@ -3,34 +3,11 @@
 {
   services.librenms = {
     enable = true;
-    nginx = {
-      enable = true;
-      serverName = "nixweb.zum.local";
-      listen = [
-        {
-          addr = "10.0.20.104";
-          port = 81;
-        }
-      ];
-    };
-
-    # Database Configuration
-    database = {
-      host = "localhost";
-      port = 3306;
-      database = "librenms";
-      username = "librenms";
-    };
 
     # Paths Configuration
     logDir = "/var/log/librenms";
     dataDir = "/var/lib/librenms";
-    hostname = "nixnms.zum.local";
-
-    # Additional Configurations
-    settings = {
-      distributed_poller = true;
-    };
+    hostname = "nixdesk.zum.local";
 
     extraConfig = ''
     '';
