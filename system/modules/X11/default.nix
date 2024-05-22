268b79757321111219f6b3355a9a78889ae358f0
dembezum <dembezuuma@gmail.com>
2024-05-22 13:13:42 +0200

    update

--- system/modules/X11/default.nix
+++ system/modules/X11/default.nix
@@ -8,6 +8,5 @@
   services.xserver.enable = true;
   services.xserver.xkb.layout = "dk"; 
 #  services.xserver.displayManager.gdm.enable = true;
-#  services.xserver.videoDrivers = [ "nvidia" "modesetting" ];
   services.xserver.videoDrivers = [ "modesetting" ];
 }
