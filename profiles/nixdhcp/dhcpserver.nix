f01ba5303951c915154c91d21f9cb3dad810f331
dembezum <dembezuuma@gmail.com>
2024-05-27 13:26:06 +0200

    update dhcp

--- profiles/nixdhcp/dhcpserver.nix
+++ profiles/nixdhcp/dhcpserver.nix
@@ -23,7 +23,7 @@
       renew-timer = 1000;
 
 # DHCP Pool configuration
-      subnet5 = [
+      subnet4 = [
       {
         subnet = "10.0.30.0/24";
         pools = [
@@ -37,20 +37,20 @@
         ];
       }
       ];
-      subnet6 = [
+    };
+      subnet4 = [
       {
-        subnet = "10.0.20.0/24";
+        subnet = "10.0.40.0/24";
         pools = [
         {
-          pool = "10.0.20.10 - 10.0.20.200";
+          pool = "10.0.40.10 - 10.0.40.200";
         }
         ];
         option-data = [
-        { name = "routers"; data = "10.0.20.2"; }
+        { name = "routers"; data = "10.0.40.2"; }
         { name = "domain-name-servers"; data = "1.1.1.1, 1.0.0.1"; }
         ];
       }
       ];
-    };
   };
 }
--- system/modules/X11/default.nix
+++ system/modules/X11/default.nix
@@ -7,5 +7,5 @@
   services.xserver.enable = true;
   services.xserver.xkb.layout = "dk"; 
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "modesetting" ];
 }
