a44392662cf9380a0a6265b197cb1868f98b04d8
dembezum <dembezuuma@gmail.com>
2024-06-01 09:37:22 +0200

    add root login to ssh

--- system/modules/ssh/default.nix
+++ system/modules/ssh/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings.PasswordAuthentication = true;
     settings.KbdInteractiveAuthentication = false;
-    settings.PermitRootLogin = "no";
+    settings.PermitRootLogin = "yes";
     ports  = [ 22 ];
   };
 }
