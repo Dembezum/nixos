70917a1bc5c7f1db323e61c2fcb3ae70bdbd33c6
dembezum <dembezuuma@gmail.com>
2024-07-28 12:30:10 +0200

    update gtk

--- user/modules/gtk/default.nix
+++ user/modules/gtk/default.nix
@@ -4,59 +4,58 @@
   home = {
     packages = with pkgs; [
       dconf
-        catppuccin-gtk
-        gnome.dconf-editor
+      catppuccin-gtk
+      dconf-editor
     ];
-#    pointerCursor = {
-#      name = "catppuccin-Mocha-Peach-cursors";
-#      package = pkgs.catppuccin-cursors.mochaPeach;
-#      size = 24;
-#      gtk.enable = true;
-#      x11.enable = true;
-#    };
+    pointerCursor = {
+      name = "catppuccin-mocha-peach-cursors";
+      package = pkgs.catppuccin-cursors.mochaPeach;
+      size = 24;
+      gtk.enable = true;
+      x11.enable = true;
+    };
   };
-
   gtk = {
     enable = true;
-    cursorTheme = {
-      package = pkgs.catppuccin-cursors.mochaSapphire;
-      name = "catppuccin-mocha-sapphire-cursors";
-    };
     theme = {
-      name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+      name = "catppuccin-mocha-lavender-standard";
       package = pkgs.catppuccin-gtk.override {
-        accents = ["lavender"];
+        accents = [ "lavender" ];
         size = "standard";
-        tweaks = [];
         variant = "mocha";
       };
     };
     iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+        '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+        '';
     };
     font = {
       name = "JetBrainsMono Nerd Font";
       size = 12;
     };
-    gtk3 = {
-      extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-    };
-    gtk4 = {
-      extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-    };
   };
 
   home.sessionVariables = {
-    GTK_THEME = "Catppuccin-Mocha-Standard-Lavender-Dark";
+    GTK_THEME = "Orchis-Dark";
   };
 
   qt = {
     enable = true;
-    platformTheme.name = "gtk";
+    platformTheme.name = "adwaita";
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
   };
 }
+
