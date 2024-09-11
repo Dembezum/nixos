04725fd7b600b556d542ca328b8cedc380a28d95
dembezum <dembezuuma@gmail.com>
2024-09-11 22:25:30 +0200

    add str1nms

---
+++ str1nms/configuration.nix
@@ -0,0 +1,79 @@
+{ systemSettings, userSettings, pkgs, ... }:
+
+{
+
+services.unifi.enable = true;
+  imports = [
+    ./librenms.nix
+    ../../universal.nix
+    ../../system/modules/ssh
+    ../../system/modules/glances
+    ./hardware-configuration.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+  php 
+  python3
+  python-pip3
+  ];
+
+  networking = {
+    nameservers = [ "1.1.1.1" ];
+    defaultGateway = {
+      address = "10.0.40.1";
+      interface = "ens18";
+    };
+    interfaces = {
+      ens18 = {
+        ipv4.addresses = [
+          {
+            address = "10.0.40.110";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 81 22 443 3306 22000 61208 61209 ];
+  };
+
+  programs.neovim = {
+    enable = true;
+    configure = {
+      customRC = ''
+        set cc=80
+        set number
+        set relativenumber
+        set hlsearch = false
+        set incsearch = true
+        set swapfile = false
+        set cursorline = true
+        set virtualedit = "block"
+        endif
+      '';
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [ ctrlp ];
+      };
+    };
+  };
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
+
+  users.users.${userSettings.username} = {
+    isNormalUser = true;
+    description = userSettings.name;
+    extraGroups = [ "plugdev" "networkmanager" "wheel" ];
+    uid = 1000;
+  };
+
+  services.qemuGuest.enable = true;
+  system.stateVersion = systemSettings.systemstate;
+}
+
---
+++ str1nms/librenms.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.librenms = {
+    enable = true;
+    user = "librenms";
+    group = "librenms";
+    hostname = "librenms.zum.local";
+    dataDir = "/var/lib/librenms";
+    logDir = "/var/log/librenms";
+
+    database = {
+      createLocally = true;
+      host = "localhost";
+      database = "librenms";
+      username = "librenms";
+      passwordFile = "/run/secret/librenms-db-password";
+    };
+
+    settings = {
+      base_url = "/";
+      top_devices = true;
+      top_ports = false;
+    };
+  };
+  }
