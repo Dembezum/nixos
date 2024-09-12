999d75aa72e69260d2c0eb290c9aa3ca46272668
dembezum <dembezuuma@gmail.com>
2024-09-12 15:33:19 +0200

    update

--- profiles/nixdhcp/dhcpserver.nix
+++ profiles/nixdhcp/dhcpserver.nix
@@ -1,56 +1,50 @@
-{ ... }:
-
 {
   services.kea = {
-    dhcp4.enable = true; 
+    dhcp4.enable = true;
     dhcp4.settings = {
-      interfaces-config = {
-        interfaces = [
-          "ens18"
-        ];
-      };
+      interfaces-config = { interfaces = [ "ens18" ]; };
 
-# Lease databse setup
+      # Lease databse setup
       lease-database = {
         name = "/var/lib/kea/dhcp4.leases";
         persist = true;
         type = "memfile";
       };
 
-# Global values 
+      # Global values 
       valid-lifetime = 4000;
       rebind-timer = 2000;
       renew-timer = 1000;
 
-# DHCP Pool configuration
-      subnet4 = [
-      {
+      # DHCP Pool configuration
+      subnet4 = [{
         subnet = "10.0.30.0/24";
-        pools = [
-        {
-          pool = "10.0.30.10 - 10.0.30.200";
-        }
-        ];
+        pools = [{ pool = "10.0.30.10 - 10.0.30.200"; }];
         option-data = [
-        { name = "routers"; data = "10.0.30.2"; }
-        { name = "domain-name-servers"; data = "1.1.1.1, 1.0.0.1"; }
+          {
+            name = "routers";
+            data = "10.0.30.2";
+          }
+          {
+            name = "domain-name-servers";
+            data = "1.1.1.1, 1.0.0.1";
+          }
         ];
-      }
-      ];
+      }];
     };
-      subnet4 = [
-      {
-        subnet = "10.0.40.0/24";
-        pools = [
+    subnet4 = [{
+      subnet = "10.0.40.0/24";
+      pools = [{ pool = "10.0.40.10 - 10.0.40.200"; }];
+      option-data = [
         {
-          pool = "10.0.40.10 - 10.0.40.200";
+          name = "routers";
+          data = "10.0.40.2";
         }
-        ];
-        option-data = [
-        { name = "routers"; data = "10.0.40.2"; }
-        { name = "domain-name-servers"; data = "1.1.1.1, 1.0.0.1"; }
-        ];
-      }
+        {
+          name = "domain-name-servers";
+          data = "1.1.1.1, 1.0.0.1";
+        }
       ];
+    }];
   };
 }
